c89c44f555b0387be370374c271c42d726c33771
JangHayoon <140604301+JangHayoon@users.noreply.github.com>
2023-07-26 19:05:44 +0900

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,92 @@
+'''
+a = 10
+b = 20
+print( a, " + ", b, " = ", a+b)
+'''
+'''
+treeHit = 0
+while treeHit < 10:
+    treeHit = treeHit +1
+    print( "나무를 %d번 찍었습니다." % treeHit )
+    if treeHit == 10:
+        print( "나무 넘어갑니다.")
+'''
+'''
+a = 9
+while a < 20:
+    a = a + 1
+    print( a, end=" ")
+   
+'''
+'''
+num = 1
+sum=0
+while num <= 10:
+   
+    sum += num # sum = sum + num
+    num = num + 1
+print("1부터 10 까지의 합 =",sum)
+print("while문이 끝난 후의 num의 값 =" , num)
+'''
+'''
+a = 10
+b = 20
+print(b,a)
+'''
+'''
+a = 10
+b = 20
+
+print(a,"+",b,"=",a+b)
+print(a+b,"+",b+b,"=",a+b+b+b)
+'''
+
+print( "원주 = 5 * 2 * 3.14 =",5 * 2 * 3.14 )
+print( "넓이 = 5 * 5 * 3.14 =",5 * 5 * 3.14 )
+
+'''
+a = 10
+b = 20
+print( a, " + ", b, " = ", a+b)
+'''
+'''
+treeHit = 0
+while treeHit < 10:
+    treeHit = treeHit +1
+    print( "나무를 %d번 찍었습니다." % treeHit )
+    if treeHit == 10:
+        print( "나무 넘어갑니다.")
+'''
+'''
+a = 9
+while a < 20:
+    a = a + 1
+    print( a, end=" ")
+   
+'''
+'''
+num = 1
+sum=0
+while num <= 10:
+   
+    sum += num # sum = sum + num
+    num = num + 1
+print("1부터 10 까지의 합 =",sum)
+print("while문이 끝난 후의 num의 값 =" , num)
+'''
+'''
+a = 10
+b = 20
+print(b,a)
+'''
+'''
+a = 10
+b = 20
+
+print(a,"+",b,"=",a+b)
+print(a+b,"+",b+b,"=",a+b+b+b)
+'''
+
+print( "원주 = 5 * 2 * 3.14 =",5 * 2 * 3.14 )
+print( "넓이 = 5 * 5 * 3.14 =",5 * 5 * 3.14 )
+
